e9d7db103616e6d474baf0760709c99bbd7cfad0
Kieran Coote-Dinh <kcoote@gmail.com>
2018-06-23 15:01:09 +1000

    Register generators with DI

--- Sequences/Startup.cs
+++ Sequences/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sequences.Logic.Generators;
+using Sequences.Logic.Interfaces;
 
 namespace Sequences
 {
@@ -22,6 +24,11 @@ namespace Sequences
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
+            services.AddTransient<ISequenceGenerator, AllNumbersSequenceGenerator>();
+            services.AddTransient<ISequenceGenerator, AllExceptMultiplesSequenceGenerator>();
+            services.AddTransient<ISequenceGenerator, EvenNumbersSequenceGenerator>();
+            services.AddTransient<ISequenceGenerator, OddNumbersSequenceGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
