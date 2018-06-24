ac19866d8e102403bc45b888db7493a033114e9b
Kieran Coote-Dinh <kcoote@gmail.com>
2018-06-25 02:36:42 +1000

    Added positive number validation to request model

--- Sequences/Models/SequenceGenerateModel.cs
+++ Sequences/Models/SequenceGenerateModel.cs
@@ -5,6 +5,7 @@ namespace Sequences.Models
     public class SequenceGenerateModel
     {
         [Required(ErrorMessage = "Number must be a positive, whole number.")]
+        [PositiveIntegerAttribute]
         public int Number { get; set; }
     }
 }
---
+++ Sequences/PositiveIntegerAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sequences
+{
+    public class PositiveIntegerAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var propertyInfo = validationContext.ObjectType.GetProperty(validationContext.MemberName);
+            int number = (int) propertyInfo.GetValue(validationContext.ObjectInstance);
+
+            if (number < 0)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} must be a positive, whole number.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
